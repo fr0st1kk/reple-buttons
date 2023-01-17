18669967a3f2aa4cd17c0109613ae3c7c67deb7a
aleksandr_brk <abk@qupe.ru>
2023-01-17 16:28:52 +0500

    add files

---
+++ js/script.js
@@ -0,0 +1,41 @@
+"use strict"
+
+// Событие клика на кнопке
+document.addEventListener("click", function (e) {
+  const targetItem = e.target;
+  if (targetItem.closest('[data-ripple]')) {
+    // Константи
+    const button = targetItem.closest('[data-ripple]');
+    const ripple = document.createElement('span');
+    const diameter = Math.max(button.clientWidth, button.clientHeight);
+    const radius = diameter / 2;
+
+    // Формирование элемента
+    ripple.style.width = ripple.style.height = `${diameter}px`;
+    ripple.style.left = `${e.pageX - (button.getBoundingClientRect().left + scrollX) - radius}px`;
+    ripple.style.top = `${e.pageY - (button.getBoundingClientRect().top + scrollY) - radius}px`;
+    ripple.classList.add('ripple');
+
+    // Удаление существующего элемента (опцыонально)
+    button.dataset.ripple === 'once' && button.querySelector('.ripple') ?
+      button.querySelector('.ripple').remove() : null;
+
+    // Добавление элемента
+    button.appendChild(ripple);
+
+    // Получение времени действия анимации
+    const timeOut = getAnimationDuration(ripple);
+
+    // Удаление элемента
+    setTimeout(() => {
+      ripple ? ripple.remove() : null;
+    }, timeOut);
+
+    // Функция получения времени действия анимации
+    function getAnimationDuration() {
+      const aDuration = window.getComputedStyle(ripple).animationDuration;
+      return aDuration.includes('ms') ?
+        aDuration.replace("ms", '') : aDuration.replace("s", '') * 1000;
+    }
+  }
+});
